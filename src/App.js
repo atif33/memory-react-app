d2ca058595f8070d7347e0e707fd7ba95df57b35
Atif Nabil <atif.nabil@modiscloud.net>
2020-10-12 03:16:24 +0200

    hide .idea

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import GuessCount from "./GuessCount";
+import Card from "./Card";
+import Atif from "./Atif";
+import shuffle from 'lodash.shuffle'
+import HallOfFame, {FAKE_HOF} from "./HalOfFame";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const SIDE = 6;
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿';
+class App extends Component {
+
+    cards = this.generateCards();
+    handleCardClicked(card) {
+        console.log('Card clicked ');
+    }
+
+    generateCards() {
+        const result = [];
+        const size = SIDE * SIDE;
+        const candidates = shuffle(SYMBOLS);
+        while (result.length < size) {
+            const card = candidates.pop();
+            result.push(card, card)
+        }
+        return shuffle(result)
+    }
+
+    render() {
+        const won = new Date().getSeconds() % 2 === 0;
+        return (<div className="memory">
+                <GuessCount guesses={0}/>
+                {this.cards.map((card, index) => (
+                    <Card card={card} feedback="visible" key={index} onClick={this.handleCardClicked} />
+                ))}
+                <Atif count={3}/>
+                {won && <HallOfFame entries={FAKE_HOF} />}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/Atif.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Atif = ({count}) => (
+    <div>
+       AtiiiF {count}
+    </div>
+);
+
+
+export default Atif
